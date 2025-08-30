c570761371e74684b54213b1e017a51972809f8a
michaelnkr808 <226908033+michaelnkr808@users.noreply.github.com>
2025-08-29 17:53:19 -0700

    Allow server to bind and listen to ports

--- server.py
+++ server.py
@@ -8,8 +8,30 @@ def create_socket():
         host = ""
         port = 9999
         s.socket.socket()
-    except:
-        print("Socket Creation Error " str(msg))
+    except socket.error as msg:
+        print("Socket Creation Error " + str(msg))
 
-#Binding socket and listening for connections
+# Binding socket and listening for connections
 
+def bind_socket():
+    try:
+        global host
+        global port
+        global s
+
+        print("Binding the port " + str(port))
+
+        s.bind((host, port))
+        s.listen(5)
+        
+    except socket.error as msg:
+        print("Socket Binding Error " + str(msg) + "\n" "Retrying...")
+        bind_socket()
+
+# Establish fection with client and socket must be listening
+
+def socket_accept():
+    conn, address = s.accept
+    print("Connection has been established | " + "ip" + address[0] + " | Port" + str(address[1]))
+    send_command(conn)
+    conn.close
