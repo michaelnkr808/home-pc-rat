df7df62f1dc9d617671285c553f1300bb01ebe30
michaelnkr808 <226908033+michaelnkr808@users.noreply.github.com>
2025-08-29 20:24:51 -0700

    Fix binding tuple issue and decode typo

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import socket
 
-HOST = ""
+HOST = "10.0.0.253"
 PORT = 9999
 
 server = socket.socket()
-server.bind(HOST, PORT)
+server.bind((HOST, PORT))
 server.listen(5)
 
 conn, addr = server.accept()
@@ -13,4 +13,4 @@ print("Connected ", addr)
 while True:
     cmd = input("Enter a command")
     conn.send(cmd.encode())
-    print(conn.recv(1024).decond)
+    print(conn.recv(1024).decode)
